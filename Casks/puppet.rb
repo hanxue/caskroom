e1211d9540003d31a5e724fac03415bdafa74c2f
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-22 21:59:57 +0800

    Initial puppet.rb Cask. BROKEN: error with path when running puppet

---
+++ Casks/puppet.rb
@@ -0,0 +1,7 @@
+class Puppet < Cask
+  url 'http://downloads.puppetlabs.com/mac/puppet-3.3.1.dmg'
+  homepage 'http://puppetlabs.com/'
+  version '3.3.1'
+  sha1 '213799d7133512f837ecae96d7a3482f836030ea'
+  install 'puppet-3.3.1.pkg'
+end
