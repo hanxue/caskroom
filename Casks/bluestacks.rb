eed3201f02b037b75d8f1e8370dfce47ba546d9e
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-07 18:34:17 +0800

    Create BlueStacks AppPlayer Cask

---
+++ Casks/bluestacks.rb
@@ -0,0 +1,7 @@
+class Bluestacks < Cask
+  url 'http://cdn.bluestacks.com/public/mac-beta/BlueStacks_AppPlayer-Beta.dmg'
+  homepage 'http://www.bluestacks.com/'
+  version 'latest'
+  no_checksum
+  link 'BlueStacks.app'
+end
