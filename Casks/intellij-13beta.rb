616f88f0dc53a9d8f1fefdc33c4a7393230dd9d4
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-07 14:45:34 +0800

    Create IntelliJ Beta v13 cask

---
+++ Casks/intellij-13beta.rb
@@ -0,0 +1,7 @@
+class Intellij13beta < Cask
+  url 'http://download.jetbrains.com/idea/ideaIU-13beta.dmg'
+  homepage 'http://confluence.jetbrains.com/display/IDEADEV/IDEA+13+EAP'
+  version 'latest'
+  no_checksum
+  link 'IntelliJ IDEA 13 EAP.app'
+end
