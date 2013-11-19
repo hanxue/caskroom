803d38783867a736e036301832e52264981ad290
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-19 16:04:57 +0800

    New Wondershare AllMyMusic Cask

---
+++ Casks/wondershare-allmymusic.rb
@@ -0,0 +1,7 @@
+class WondershareAllmymusic < Cask
+  url 'http://download.handysoft.org/ws_allmymusic_mac_av.dmg'
+  homepage 'http://spotifydownloader.net/'
+  version 'latest'
+  no_checksum
+  link 'Wondershare AllMyMusic.app'
+end
