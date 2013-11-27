b2a062bb8713e9158fb9b50312e1dc3988815466
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-27 14:20:39 +0800

    New imgurBar Cask

---
+++ Casks/imgurbar.rb
@@ -0,0 +1,7 @@
+class Imgurbar < Cask
+  url 'https://github.com/zbuc/imgurBar/raw/master/imgur.dmg'
+  homepage 'https://github.com/zbuc/imgurBar'
+  version 'latest'
+  no_checksum
+  link 'imgur.app'
+end
