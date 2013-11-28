299b758c7a16d900a81c1aba3383316fb08ebb99
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-28 22:02:35 +0800

    Add Leonhard 0.9.1

---
+++ Casks/leonhard.rb
@@ -0,0 +1,7 @@
+class Leonhard < Cask
+  url 'https://www.macupdate.com/download/37297/Leonhard-0.9.1.dmg'
+  homepage 'http://algorithmique.net/leonhard.html'
+  version '0.9.1'
+  sha1 '42bcabbe5c69db6cc149fede1a72f24b7caa8754'
+  link 'Leonhard.app'
+end
