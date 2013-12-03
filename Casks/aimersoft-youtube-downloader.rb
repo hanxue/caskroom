473d56f5ccc1fcab2220c03a3faf85b9739dd42c
Lee Hanxue <hanxue.developer@gmail.com>
2013-12-03 18:02:29 +0800

    Add Aimersoft YouTube Downloader 4.5.0 cask

---
+++ Casks/aimersoft-youtube-downloader.rb
@@ -0,0 +1,7 @@
+class AimersoftYoutubeDownloader < Cask
+  url 'http://download.aimersoft.com/aimer-mac-youtube-downloader-intel_full635.dmg'
+  homepage 'http://www.aimersoft.com/youtube-downloader.html'
+  version '4.5.0'
+  sha1 'bef92e56d7eda56c9673c53cd087f5a93c205653'
+  link 'Aimersoft YouTube Downloader.app'
+end
