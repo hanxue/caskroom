143c96234c8cdc5463619816ee1e07bd2d048ce3
Lee Hanxue <hanxue.developer@gmail.com>
2014-01-03 10:34:56 +0800

    Wowza media server 3.6.3

---
+++ Casks/wowza-media-server.rb
@@ -0,0 +1,7 @@
+class WowzaMediaServer < Cask
+  url 'http://www.wowza.com/downloads/WowzaMediaServer-3-6-3/WowzaMediaServer-3.6.3.dmg'
+  homepage 'http://www.wowza.com/'
+  version '3.6.3'
+  sha1 '25d5257108f06513b971613ab3e5f732123a388c'
+  install 'WowzaMediaServer-3.6.3.pkg'
+end
