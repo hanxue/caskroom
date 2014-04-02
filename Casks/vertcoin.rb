f48909a9e3ad2d5941dc12d1d5ac170443abe261
Lee Hanxue <hanxue.developer@gmail.com>
2014-04-02 20:30:48 +0800

    Add Vertcoin 0.8.6.3

---
+++ Casks/vertcoin.rb
@@ -0,0 +1,7 @@
+class Vertcoin < Cask
+  url 'https://github.com/vertcoin/vertcoin/releases/download/v0.8.6.3/Vertcoin-Qt-v0.8.6.3.dmg'
+  homepage 'https://vertcoin.org/'
+  version '0.8.6.3'
+  sha256 'a00d392d904e70d6fb08d86ec65207b0bf1182705e7e08e74016720f0b1cd99e'
+  link 'Vertcoin-Qt.app'
+end
